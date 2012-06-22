56641a095816cbe4d23b8a722008c202c095430d
Peter Hultqvist <phq@silentorbit.com>
2012-06-22 18:34:51 +0200

    Fixed warning if directories exist

--- graph-all.sh
+++ graph-all.sh
@@ -3,11 +3,11 @@
 # csm is assumed to be located in the root/home directory
 # of a user-account of its own.
 #cd ~
-mkdir images
-mkdir images/hour
-mkdir images/day
-mkdir images/week
-mkdir images/month
+mkdir -p images
+mkdir -p images/hour
+mkdir -p images/day
+mkdir -p images/week
+mkdir -p images/month
 ./graph.py 1	0	images/hour
 ./graph.py 24	0	images/day
 ./graph.py 168	0	images/week

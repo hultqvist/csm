cf5088c5caa0c3ff7df9564aad436ebd24c15bbd
Peter Hultqvist <phq@silentorbit.com>
2012-06-22 03:21:02 +0200

    Added sample php index file for generated graphs

---
+++ images/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Server usage</title>
+	<style>
+/*to make all 4 plots fall in one line*/
+body{
+	width: 2000px;
+}
+	</style>
+</head>
+<body>
+<?php
+error_reporting (E_ALL);
+
+$files = array();
+
+$dh = opendir("hour");
+while(true)
+{
+	$file = readdir($dh);
+	if($file === false)
+		break;
+	if($file == ".")
+		continue;
+	if($file == "..")
+		continue;
+	if($file == "index.php")
+		continue;
+
+	$files[] = $file;
+}
+
+sort($files);
+
+foreach($files as $file)
+{
+	echo '<p>'.$file.'</p>';
+	echo '<img src="hour/'.$file.'"/>';
+	echo '<img src="day/'.$file.'"/>';
+	echo '<img src="week/'.$file.'"/>';
+	echo '<img src="month/'.$file.'"/>';
+}
+
+?>
+</body>
+</html>
